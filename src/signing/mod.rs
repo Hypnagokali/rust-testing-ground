fc8077c8a1818fe83668be52feaa8f3ef23819a8
Stefan Simon <stefan.j.c.simon@gmail.com>
2025-02-21 20:36:51 +0100

    [*] Base structure for validating JWT

--- src/signing/mod.rs
+++ src/signing/mod.rs
@@ -1,2 +1,3 @@
 pub mod sign_lib;
 pub mod keys;
+pub mod jwt;
--- src/signing/sign_lib.rs
+++ src/signing/sign_lib.rs
@@ -1,5 +1,5 @@
 use rsa::{pkcs8::{DecodePrivateKey, DecodePublicKey}, Pkcs1v15Sign, RsaPrivateKey, RsaPublicKey};
-use sha2::{digest::{consts::U32, generic_array::{ArrayLength, GenericArray}}, Digest, Sha256};
+use sha2::{digest::{consts::U32, generic_array::GenericArray}, Digest, Sha256};
 
 use super::keys::{PRIVATE_KEY, PUBKEY};
 
@@ -32,8 +32,19 @@ pub fn verify_message(msg: &str, signature: &Vec<u8>) -> bool {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
+    use jsonwebtoken::{decode, errors::ErrorKind, Algorithm, DecodingKey, Validation};
+    use serde::Deserialize;
+
+    use crate::signing::jwt::{test_e, test_n, token};
+
     use super::{sign_message, verify_message};
 
+    #[derive(Deserialize)]
+    struct Claims {
+        name: String,
+    }
 
     #[test]
     fn test_signing() {
@@ -43,6 +54,20 @@ mod tests {
         let is_authentic = verify_message(msg, &signature);
 
         assert!(is_authentic);
+    }
+
+    #[test]
+    fn test_verify_token() {
+        // just hardcoded for now
+        let jwt = token;
+
+        let decoding_key = DecodingKey::from_rsa_components(test_n, test_e).unwrap();
+
+        let validation = Validation::new(Algorithm::RS256);
+        let decoded = decode::<Claims>(jwt, &decoding_key, &validation);
+
+        let e = decoded.err().unwrap();
+        assert_eq!(e.kind(), &ErrorKind::ExpiredSignature);
     }
 
 }
